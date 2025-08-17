8284f5f390f43b51b6c430d366b9f17c5c4fe198
Dechateau2035 <dechateaufoyou@gmail.com>
2025-08-17 05:22:54 +0100

    17/08/2025

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MonSite/',
+  //base: '/MonSite/',
   plugins: [react()],
   server: {
     hmr: true,
